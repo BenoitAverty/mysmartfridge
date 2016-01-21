d80f3bebb24bfb1e1807986b35330ac06a7395e1
Benoit AVERTY <benoit.averty@gmail.com>
2016-01-21 14:01:28 +0100

    initial commit

---
+++ src/main/java/com/mysmartfridge/MySmartFridgeApplication.java
@@ -0,0 +1,12 @@
+package com.mysmartfridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySmartFridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySmartFridgeApplication.class, args);
+	}
+}
