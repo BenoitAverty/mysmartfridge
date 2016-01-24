b82d443a4043ece4c01167bd6745f12825897dae
Benoit AVERTY <benoit.averty@gmail.com>
2016-01-24 19:56:03 +0100

    improve toolchain

--- src/main/java/com/mysmartfridge/MySmartFridgeApplication.java
+++ src/main/java/com/mysmartfridge/MySmartFridgeApplication.java
@@ -1,5 +1,8 @@
 package com.mysmartfridge;
 
+import java.net.URL;
+import java.net.URLClassLoader;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +10,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MySmartFridgeApplication {
 
 	public static void main(String[] args) {
+		ClassLoader cl = ClassLoader.getSystemClassLoader();
+
+        URL[] urls = ((URLClassLoader)cl).getURLs();
+
+        for(URL url: urls){
+        	System.out.println(url.getFile());
+        }
 		SpringApplication.run(MySmartFridgeApplication.class, args);
 	}
 }
--- src/main/webapp/Gruntfile.js
+++ src/main/webapp/Gruntfile.js
@@ -1,19 +1,22 @@
 module.exports = function(grunt) {
+  var app_src_root="app/";
+  var app_dist_root="../resources/public/"
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     browserify: {
       js: {
-        src: 'app/js/mysmartfridge.js',
-        dest: 'dist/js/mysmartfridge.js'
+        src: app_src_root+'js/mysmartfridge.js',
+        dest: app_dist_root+'js/mysmartfridge.js'
       }
     },
     sass: {
       dist: {
         files: [{
           expand: true,
-          cwd: 'app/sass/',
+          cwd: app_src_root+'sass/',
           src: ['mysmartfridge.scss'],
-          dest: 'dist/css',
+          dest: app_dist_root+'css/',
           ext: '.css'
         }]
       }
@@ -28,18 +31,41 @@ module.exports = function(grunt) {
         filter: 'isFile',
         ext: '.scss'
       },
-      appCss: {
+      appHtml: {
         expand: true,
-        cwd: 'app/',
-        src: ['**/*.html', '**/*.css'],
-        dest: 'dist/',
+        cwd: app_src_root,
+        src: ['**/*.html'],
+        dest: app_dist_root,
       }
+    },
+    defaultWatch: {
+      js: {
+        files: [app_src_root+'**/*.js'],
+        tasks: ['browserify']
+      },
+      sass: {
+        files: [app_src_root+'**/*.scss',],
+        tasks: ['sass']
+      },
+      html: {
+        files: [app_src_root+'**/*.html'],
+        tasks: ['copy:appHtml']
+      },
+    },
+    clean: {
+      app: [app_dist_root],
+      options: {force: true}
     }
   });
 
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
-  grunt.registerTask('build', ['copy:cssAsScss', 'browserify', 'sass', 'copy:appCss']);
+  grunt.registerTask('build', ['copy:cssAsScss', 'browserify', 'sass', 'copy:appHtml']);
+
+  grunt.renameTask('watch', 'defaultWatch');
+  grunt.registerTask('watch', ['build', 'defaultWatch']);
 }
