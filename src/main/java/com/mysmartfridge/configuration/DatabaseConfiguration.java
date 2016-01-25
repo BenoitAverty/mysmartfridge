7f8aa1b3c4ebd51799b0436195073151b9067599
Benoit AVERTY <benoit.averty@gmail.com>
2016-01-25 18:13:58 +0100

    Added liquibase setup for the project.

---
+++ src/main/java/com/mysmartfridge/configuration/DatabaseConfiguration.java
@@ -0,0 +1,44 @@
+package com.mysmartfridge.configuration;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.h2.tools.Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.mysmartfridge.MySmartFridgeApplication;
+
+import liquibase.integration.spring.SpringLiquibase;
+
+@Configuration
+@EnableJpaRepositories("com.mysmartfridge.domain")
+@EnableTransactionManagement
+public class DatabaseConfiguration {
+
+    private final Logger log = LoggerFactory.getLogger(DatabaseConfiguration.class);
+
+    /**
+     * Open the TCP port for the H2 database, so it is available remotely.
+     */
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    @Profile(MySmartFridgeApplication.PROFILE_DEV)
+    public Server h2TCPServer() throws SQLException {
+        return Server.createTcpServer("-tcp","-tcpAllowOthers");
+    }
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog("classpath:liquibase/master.xml");
+        log.debug("Configuring Liquibase");
+        return liquibase;
+    }
+}
--- src/main/java/com/mysmartfridge/domain/Recipe.java
+++ src/main/java/com/mysmartfridge/domain/Recipe.java
@@ -6,6 +6,7 @@ import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import org.hibernate.annotations.Type;
 
@@ -13,6 +14,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name="Recipes")
 @NoArgsConstructor
 public class Recipe implements Serializable {
 
