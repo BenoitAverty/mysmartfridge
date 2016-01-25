641e7580984072c4044be29c66ebed24f807d3fb
Benoit AVERTY <benoit.averty@gmail.com>
2016-01-25 14:27:34 +0100

    Moved webapp directly in src/main/webapp/ directory. Compilation is now in-place

--- src/main/webapp/Gruntfile.js
+++ src/main/webapp/Gruntfile.js
@@ -1,26 +1,36 @@
 module.exports = function(grunt) {
-  var app_src_root="app/";
-  var app_dist_root="../resources/public/"
 
   grunt.initConfig({
+    // Read package config from npm module
     pkg: grunt.file.readJSON('package.json'),
+
+    // Javascript compilation. The main javascript file is mysmartfrifge.js, and is compiled
+    // into mysmartfridge.dist.js. This is the only file that has to be included in the index.html.
     browserify: {
       js: {
-        src: app_src_root+'js/mysmartfridge.js',
-        dest: app_dist_root+'js/mysmartfridge.js'
+        src: 'js/mysmartfridge.js',
+        dest: 'js/mysmartfridge.dist.js'
+      },
+      options: {
+        browserifyOptions: {
+          debug: true
+        }
       }
     },
+
+    // Sass compilation. Files in the sass/ directory are compiled and put into the css/ directory.
     sass: {
       dist: {
         files: [{
           expand: true,
-          cwd: app_src_root+'sass/',
+          cwd: 'sass/',
           src: ['mysmartfridge.scss'],
-          dest: app_dist_root+'css/',
+          dest: 'css/',
           ext: '.css'
         }]
       }
     },
+
     copy : {
       // Workaround to be able to import css files with sass using sass import and not W3C import feature.
       cssAsScss: {
@@ -31,30 +41,23 @@ module.exports = function(grunt) {
         filter: 'isFile',
         ext: '.scss'
       },
-      appHtml: {
-        expand: true,
-        cwd: app_src_root,
-        src: ['**/*.html'],
-        dest: app_dist_root,
-      }
     },
-    defaultWatch: {
+
+    // Settings for watch task. It is separated in several parts to avoid a complete recompilation each time a file changes.
+    watch: {
       js: {
-        files: [app_src_root+'**/*.js'],
+        files: ['**/*.js', '!**/*.dist.js'],
         tasks: ['browserify']
       },
       sass: {
-        files: [app_src_root+'**/*.scss',],
+        files: ['**/*.scss'],
         tasks: ['sass']
-      },
-      html: {
-        files: [app_src_root+'**/*.html'],
-        tasks: ['copy:appHtml']
-      },
+      }
     },
+
+    // Clean task. Remove every generated file.
     clean: {
-      app: [app_dist_root],
-      options: {force: true}
+      app: ['css/', 'js/mysmartfridge.dist.js']
     }
   });
 
@@ -64,8 +67,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-clean');
 
-  grunt.registerTask('build', ['copy:cssAsScss', 'browserify', 'sass', 'copy:appHtml']);
+  grunt.registerTask('build', ['copy:cssAsScss', 'browserify', 'sass']);
 
-  grunt.renameTask('watch', 'defaultWatch');
-  grunt.registerTask('watch', ['build', 'defaultWatch']);
+  grunt.registerTask('build-watch', ['build', 'watch']);
 }
