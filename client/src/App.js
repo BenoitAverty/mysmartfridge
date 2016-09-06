f472165fb8912f721efa86d30d47cd91df395166
Benoit Averty <benoit.averty@gmail.com>
2016-09-06 18:40:29 +0200

    Bootstrap client + server

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      greeting: 'Loading...',
+    };
+  }
+
+  componentDidMount() {
+    fetch('/hello')
+      .then(r => r.text())
+      .then(data => this.setState({ greeting: data }));
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,7 +24,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.state.greeting}
         </p>
       </div>
     );
